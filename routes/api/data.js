6a5e96bfee4cb0c743fa8f16446bd6473f5c0438
Augyu <yuhsuan@vt.edu>
2019-11-23 21:18:43 -0500

    pg client

--- routes/api/data.js
+++ routes/api/data.js
@@ -6,21 +6,18 @@ require('dotenv').config();
 
 /* postgresql connection */
 const { Client } = require('pg')
-
 const connectionString = process.env.DATABASE_URL
-  
 const client = new Client({
   connectionString: connectionString,
   ssl: true
 })
-
+client.connect()
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   try {
-    client.connect()
     const result = await client.query('SELECT * FROM dcrealcrime')
     res.json(result.rows)
-    client.end()
+    //client.end()
   } catch (err) {
     console.log(err.stack)
   }
