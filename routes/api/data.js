4d32de43ff6937d5cc214c655bde9a9ad31fa1b6
Augyu <yuhsuan@vt.edu>
2019-12-01 17:06:13 -0500

    Crime Data API

--- routes/api/data.js
+++ routes/api/data.js
@@ -15,7 +15,7 @@ client.connect()
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   try {
-    const result = await client.query('SELECT * FROM dcrealcrime')
+    const result = await client.query('SELECT x, y, report_dat, tweet_text, dcrealcrime.offense, is_twitter FROM dcrealcrime LEFT JOIN twitterdata ON dcrealcrime.octo_record_id2 =  twitterdata.octo_record_id')
     res.json(result.rows)
     //client.end()
   } catch (err) {
@@ -26,7 +26,17 @@ router.get('/', async function(req, res, next) {
 router.get('/:startTime/:endTime/:location/:crimeType', async function(req, res){
   res.send(req.params)
 })
-// export const findEvent = (start, end ,location , type) => {
+// location to lat and long
+const reverseGeocode = (location) => {
+  axios
+      .get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key='+process.env.REACT_APP_GOOGLE_API)
+      .then(data => this.setState({ latAndLong: data}))
+      .catch(err => {
+        console.log(err);
+        return null;
+      })
+}
+// const findEvent = (start, end, lat, long, type) => {
 //   const result = await client.query('')
 // }
 module.exports = router;
